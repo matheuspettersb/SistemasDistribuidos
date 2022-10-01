a6b3a6b0fcf04574d412735bb335b320123cca77
Matheus Petters Bevilaqua <73998677+matheuspettersb@users.noreply.github.com>
2022-10-01 10:33:44 -0300

    Update Server.java

--- Server.java
+++ Server.java
@@ -5,11 +5,12 @@ import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Server extends Thread {
+public class RelogioServer extends Thread {
 	public boolean liberado = true;
 	public static ArrayList<Socket> fila = new ArrayList<Socket>(); // fila de sockets - conexao
 	public static Socket cliente;
@@ -18,6 +19,7 @@ public class Server extends Thread {
 	public static PrintWriter out;
 	public static BufferedReader in;
 	public static int indexAtual = 0;
+	public static LocalTime relogio;
 
 	// adiciona os clientes na fila
 	public void run() {
@@ -47,8 +49,7 @@ public class Server extends Thread {
 
 	public static void main(String[] args) throws Exception {
 		String mensagem;
-		String mensagemQuadro;
-		String quadro = "##### QUADRO DE AVISOS #####\n";
+		relogio = LocalTime.now();
 
 		// inicializa o servidor
 		servidor = new ServerSocket(8080);
@@ -56,21 +57,39 @@ public class Server extends Thread {
 
 		while (true) {
 			// se n tiver ninguem na fila, adiciona
+			// ### mudar a logica aqui, usar um tempo limite pra conexão(?)
 			if (fila.size() == 0) {
 				novoCliente = servidor.accept();
 				fila.add(novoCliente);
 			}
-
-			if (fila.size() > 0) {
-				Server thread = new Server(); // começa um objeto dessa propria classe
+			// ###
+			
+			// entrada e saida
+			out = new PrintWriter(cliente.getOutputStream(), true);
+			in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+			
+			int i=0;
+			while (i!=fila.size()) {
+				//envia a hora do server pro cliente
+				System.out.println("Requisitando a hora do cliente " + (i+1));
+				cliente = fila.get(indexAtual);
+				out.print(relogio);
+				
+				//cliente devolve a diferença de tempo
+				i++;
+			}
+			
+			//referencia do codigo antigo
+			//if (fila.size() > 0) {
+				RelogioServer thread = new RelogioServer(); // começa um objeto dessa propria classe
 				thread.start();
 				cliente = fila.get(indexAtual);
 
-				System.out.println("Novo cliente conectado: " + servidor.toString());
+				System.out.println("Novo relogio conectado: " + cliente.toString());
 
-				// entrada e saida
-				out = new PrintWriter(cliente.getOutputStream(), true);
-				in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+//				// entrada e saida
+//				out = new PrintWriter(cliente.getOutputStream(), true);
+//				in = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
 				
 				while ((mensagem = in.readLine()) != null) { // *
 					System.out.println("recebeu: "+mensagem);
@@ -78,9 +97,9 @@ public class Server extends Thread {
 						out.println("acesso permitido");
 					}
 //					out.println("acesso permitido");
-					mensagemQuadro = in.readLine();
-					quadro = (quadro + "\n" + mensagemQuadro);
-					System.out.println(quadro);
+//					mensagemQuadro = in.readLine();
+//					quadro = (quadro + "\n" + mensagemQuadro);
+//					System.out.println(quadro);
 				}		
 
 				// terminando
@@ -90,84 +109,8 @@ public class Server extends Thread {
 				out.close();
 				in.close();
 				thread.interrupt();
-			}
+			//}
 		}
 	}
 }
 
-//		if (server.isLiberado()) {
-//			try {
-//				// liberado
-//				Socket socket;
-//
-//				if (server.fila.isEmpty()) {
-//					socket = servidor.accept();
-//				} else {
-//					socket = server.fila.remove(0);
-//				}
-//				System.out.println("Cliente conectou: " + socket.getInetAddress().getHostName());
-//				server.setLiberado(false);
-//				System.out.println("Fechando o Acesso");
-//				System.out.println(server.isLiberado());
-//
-//				server.usandoQuadro(socket);
-//
-//				server.setLiberado(true);
-//				System.out.println("\nAcesso Liberado novamente");
-//
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//
-//		} else {
-//			// não liberado
-//			// adiciona na lista + processo aguarda
-//			// (?) - armazenando o endereço do client pra
-//			// puxar depois (?)
-//
-//			System.out.println("Acesso negado - Adicionando socket a lista");
-//			try {
-//				Socket proximo = servidor.accept();
-//				server.fila.add(proximo);
-//			} catch (IOException e1) {
-//				e1.printStackTrace();
-//			}
-//
-//		}
-//	}
-//
-//	}}.start();
-//// 	}
-//
-//	public void usandoQuadro(Socket socket) {
-//		System.out.println(quadro);
-//		Scanner leitura;
-//		try {
-//			leitura = new Scanner(socket.getInputStream());
-//			while (leitura.hasNext()) {
-//				String texto = leitura.nextLine();
-//				System.err.println(texto);
-//				quadro = quadro + ("\n" + texto);
-//			}
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
-//
-////	public boolean isLiberado() {
-////		return liberado;
-////	}
-//
-//	public void setLiberado(boolean liberado) {
-//		this.liberado = liberado;
-//	}
-//
-//	public String getQuadro() {
-//		return quadro;
-//	}
-//
-//	public void setQuadro(String quadro) {
-//		this.quadro = quadro;
-//	}
-//
-//}
